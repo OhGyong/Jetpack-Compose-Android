c67db9c2f60ac5950d33846ee7a5730423a3864f
O_Gyong <ogyong7143@gmail.com>
2023-05-02 15:09:08 +0900

    PATHWAY 1- MigrationCodelab + 메인 이미지 적용

--- PATHWAY 1- Compose essentials/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ PATHWAY 1- Compose essentials/MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -19,8 +19,10 @@ package com.google.samples.apps.sunflower.plantdetail
 import android.service.autofill.TextValueSanitizer
 import android.text.method.LinkMovementMethod
 import android.widget.TextView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
@@ -33,35 +35,26 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.text.HtmlCompat
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.google.accompanist.themeadapter.material.MdcTheme
 import com.google.samples.apps.sunflower.R
 import com.google.samples.apps.sunflower.data.Plant
 import com.google.samples.apps.sunflower.viewmodels.PlantDetailViewModel
 
-@Composable
-private fun PlantDescription(description: String) {
-    val htmlDescription = remember(description) {
-        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
-    }
 
-    AndroidView(
-        factory = { context->
-            TextView(context).apply {
-                movementMethod = LinkMovementMethod.getInstance()
-            }
-        },
-        update = {
-            it.text = htmlDescription
-        }
-    )
-}
 
 @Composable
 fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
@@ -79,12 +72,31 @@ fun PlantDetailDescription(plantDetailViewModel: PlantDetailViewModel) {
 fun PlantDetailContent(plant: Plant) {
     Surface {
         Column(Modifier.padding(dimensionResource(id = R.dimen.margin_normal))) {
+            PlantImage(uri = plant.imageUrl)
             PlantName(name = plant.name)
             PlantWatering(wateringInterval = plant.wateringInterval)
             PlantDescription(description = plant.description)
         }
     }
 }
+
+@Composable
+private fun PlantImage(uri: String) {
+    Image(
+        painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(uri)
+                .crossfade(true)
+                .build()
+        ),
+        contentDescription = null,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(278.dp),
+        contentScale = ContentScale.Crop
+    )
+}
+
 
 @Composable
 private fun PlantName(name: String) {
@@ -123,6 +135,24 @@ private fun PlantWatering(wateringInterval: Int) {
             modifier = centerWithPaddingModifier.padding(bottom = normalPadding)
         )
     }
+}
+
+@Composable
+private fun PlantDescription(description: String) {
+    val htmlDescription = remember(description) {
+        HtmlCompat.fromHtml(description, HtmlCompat.FROM_HTML_MODE_COMPACT)
+    }
+
+    AndroidView(
+        factory = { context->
+            TextView(context).apply {
+                movementMethod = LinkMovementMethod.getInstance()
+            }
+        },
+        update = {
+            it.text = htmlDescription
+        }
+    )
 }
 
 @Preview
