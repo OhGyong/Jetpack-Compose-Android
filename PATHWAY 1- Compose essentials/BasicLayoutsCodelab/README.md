df25e436e0337ea2dda01467f04834032454f023
O_Gyong <ogyong7143@gmail.com>
2023-05-02 17:51:03 +0900

    PATHWAY 1- BasicLayoutsCodelab 6 (UI만 작업)

--- PATHWAY 1- Compose essentials/BasicLayoutsCodelab/README.md
+++ PATHWAY 1- Compose essentials/BasicLayoutsCodelab/README.md
@@ -1,3 +1,11 @@
 # [Basic layouts in Compose](https://developer.android.com/codelabs/jetpack-compose-layouts?hl=ko&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fcompose%3Fhl%3Dko%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fjetpack-compose-layouts#0)
 
 이 프로젝트는 android-compose-codelabs의 [BasicLayoutsCodelab](https://github.com/googlecodelabs/android-compose-codelabs/tree/main/BasicLayoutsCodelab) 프로젝트를 바탕으로 Codelab에 맞춰 진행하였습니다.
+
+## 1~3. 프로젝트 소개, 설정, 계획
+UI 도구 키트인 Compose를 사용하면 앱 디자인을 쉽게 구현할 수 있다.
+개발자가 UI의 디자인을 기술하면 Compose가 화면에 그리는 작업을 처리한다.
+이 codelab에서는 앞에서 배운 것 보다 현실적이고 복잡한 레이아웃을 구현해볼 것이다.
+
+앱을 개발하기 위한 디자인을 받았을 때 먼저 디자인의 구조를 명확하게 파악하는 것이 좋다.
+> 디자인을 분석하여 UI를 여러 개의 재사용 가능한 부분으로 나누는 등의 고민을 해보자. 
--- PATHWAY 1- Compose essentials/BasicLayoutsCodelab/app/src/main/java/com/study/basiclayoutscodelab/MainActivity.kt
+++ PATHWAY 1- Compose essentials/BasicLayoutsCodelab/app/src/main/java/com/study/basiclayoutscodelab/MainActivity.kt
@@ -3,26 +3,48 @@ package com.study.basiclayoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.study.basiclayoutscodelab.ui.theme.BasicLayoutsCodelabTheme
+import com.study.basiclayoutscodelab.ui.theme.typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BasicLayoutsCodelabTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
                 ) {
-                    Greeting("Android")
+                    val modifier = Modifier
+                    MainView(modifier)
                 }
             }
         }
@@ -30,17 +52,115 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun MainView(modifier: Modifier) {
+    Column(
         modifier = modifier
+            .padding(top = 16.dp, start = 8.dp, end = 8.dp)
+    ) {
+        SearchBar(modifier = modifier)
+        AlignYourBodyElement(modifier)
+        FavoriteCollectionCard(modifier)
+    }
+
+}
+
+@Composable
+fun SearchBar(modifier: Modifier) {
+    TextField(
+        value = "",
+        onValueChange = {},
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = null,
+                modifier = Modifier.size(16.dp)
+            )
+        },
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.surface
+        ),
+        placeholder = {
+            Text(stringResource(R.string.placeholder_search))
+        },
+        modifier = modifier
+            .fillMaxWidth()
+            .heightIn(min = 56.dp)
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BasicLayoutsCodelabTheme {
-        Greeting("Android")
+fun AlignYourBodyElement(modifier: Modifier) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ab1_inversions),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = R.string.ab1_inversions),
+            style = typography.h3,
+            modifier = modifier
+                .paddingFromBaseline(top = 24.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun FavoriteCollectionCard(modifier: Modifier) {
+    Surface(
+        shape = MaterialTheme.shapes.small,
+        modifier = modifier
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier.width(192.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.fc2_nature_meditations),
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = modifier.size(56.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.fc2_nature_meditations),
+                style = typography.h3,
+                modifier = modifier.padding(horizontal = 16.dp)
+            )
+        }
+    }
+}
+
+@Preview(
+    widthDp = 360,
+    heightDp = 640,
+    showBackground = true,
+    backgroundColor = 0xFFF0EAE2
+)
+@Composable
+fun MainViewPreview() {
+    MaterialTheme {
+        MainView(Modifier)
+    }
+}
+
+@Preview
+@Composable
+fun SearchBarPreview() {
+    MaterialTheme {
+        SearchBar(modifier = Modifier)
+    }
+}
+
+@Preview
+@Composable
+fun AlignYourBodyElementPreview() {
+    MaterialTheme {
+        AlignYourBodyElement(modifier = Modifier)
     }
 }
