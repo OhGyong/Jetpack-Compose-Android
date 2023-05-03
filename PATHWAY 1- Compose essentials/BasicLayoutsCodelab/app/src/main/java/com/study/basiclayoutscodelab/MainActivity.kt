ed2328d97f9651cbc189838ae462ec8066690670
O_Gyong <ogyong7143@gmail.com>
2023-05-03 14:26:38 +0900

    PATHWAY 1- BasicLayoutsCodelab 7,8 (UI만 작업)

--- PATHWAY 1- Compose essentials/BasicLayoutsCodelab/app/src/main/java/com/study/basiclayoutscodelab/MainActivity.kt
+++ PATHWAY 1- Compose essentials/BasicLayoutsCodelab/app/src/main/java/com/study/basiclayoutscodelab/MainActivity.kt
@@ -3,16 +3,26 @@ package com.study.basiclayoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
@@ -34,6 +44,27 @@ import androidx.compose.ui.unit.dp
 import com.study.basiclayoutscodelab.ui.theme.BasicLayoutsCodelabTheme
 import com.study.basiclayoutscodelab.ui.theme.typography
 
+private val alignYourBodyData = listOf(
+    R.drawable.ab1_inversions to R.string.ab1_inversions,
+    R.drawable.ab2_quick_yoga to R.string.ab2_quick_yoga,
+    R.drawable.ab3_stretching to R.string.ab3_stretching,
+    R.drawable.ab4_tabata to R.string.ab4_tabata,
+    R.drawable.ab5_hiit to R.string.ab5_hiit,
+    R.drawable.ab6_pre_natal_yoga to R.string.ab6_pre_natal_yoga
+).map { DrawableStringPair(it.first, it.second) }
+
+private val favoriteCollectionsData = listOf(
+    R.drawable.fc1_short_mantras to R.string.fc1_short_mantras,
+    R.drawable.fc2_nature_meditations to R.string.fc2_nature_meditations,
+    R.drawable.fc3_stress_and_anxiety to R.string.fc3_stress_and_anxiety,
+    R.drawable.fc4_self_massage to R.string.fc4_self_massage,
+    R.drawable.fc5_overwhelmed to R.string.fc5_overwhelmed,
+    R.drawable.fc6_nightly_wind_down to R.string.fc6_nightly_wind_down
+).map { DrawableStringPair(it.first, it.second) }
+private data class DrawableStringPair(
+    @DrawableRes val drawable: Int,
+    @StringRes val text: Int
+)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -58,8 +89,8 @@ fun MainView(modifier: Modifier) {
             .padding(top = 16.dp, start = 8.dp, end = 8.dp)
     ) {
         SearchBar(modifier = modifier)
-        AlignYourBodyElement(modifier)
-        FavoriteCollectionCard(modifier)
+        AlignYourBodyRow(modifier)
+        FavoriteCollectionsGrid(modifier)
     }
 
 }
@@ -89,13 +120,30 @@ fun SearchBar(modifier: Modifier) {
 }
 
 @Composable
-fun AlignYourBodyElement(modifier: Modifier) {
+fun AlignYourBodyRow(modifier: Modifier) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp) ,
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(alignYourBodyData) { item ->
+            AlignYourBodyElement(item.drawable, item.text, modifier)
+        }
+    }
+}
+
+@Composable
+fun AlignYourBodyElement(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier
+) {
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
         Image(
-            painter = painterResource(id = R.drawable.ab1_inversions),
+            painter = painterResource(drawable),
             contentDescription = null,
             contentScale = ContentScale.Crop,
             modifier = Modifier
@@ -103,7 +151,7 @@ fun AlignYourBodyElement(modifier: Modifier) {
                 .clip(CircleShape)
         )
         Text(
-            text = stringResource(id = R.string.ab1_inversions),
+            text = stringResource(text),
             style = typography.h3,
             modifier = modifier
                 .paddingFromBaseline(top = 24.dp, bottom = 8.dp)
@@ -112,7 +160,28 @@ fun AlignYourBodyElement(modifier: Modifier) {
 }
 
 @Composable
-fun FavoriteCollectionCard(modifier: Modifier) {
+fun FavoriteCollectionsGrid(
+    modifier: Modifier
+) {
+    LazyHorizontalGrid(
+        rows = GridCells.Fixed(2),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier.height(120.dp)
+    ) {
+        items(favoriteCollectionsData) { item ->
+            FavoriteCollectionCard(item.drawable, item.text, modifier = modifier.height(56.dp))
+        }
+    }
+}
+
+@Composable
+fun FavoriteCollectionCard(
+    @DrawableRes drawable: Int,
+    @StringRes text: Int,
+    modifier: Modifier
+) {
     Surface(
         shape = MaterialTheme.shapes.small,
         modifier = modifier
@@ -122,13 +191,13 @@ fun FavoriteCollectionCard(modifier: Modifier) {
             modifier = modifier.width(192.dp)
         ) {
             Image(
-                painter = painterResource(id = R.drawable.fc2_nature_meditations),
+                painter = painterResource(id = drawable),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = modifier.size(56.dp)
             )
             Text(
-                text = stringResource(id = R.string.fc2_nature_meditations),
+                text = stringResource(id = text),
                 style = typography.h3,
                 modifier = modifier.padding(horizontal = 16.dp)
             )
@@ -161,6 +230,10 @@ fun SearchBarPreview() {
 @Composable
 fun AlignYourBodyElementPreview() {
     MaterialTheme {
-        AlignYourBodyElement(modifier = Modifier)
+        AlignYourBodyElement(
+            text = R.string.ab1_inversions,
+            drawable = R.drawable.ab1_inversions,
+            modifier = Modifier
+        )
     }
 }
