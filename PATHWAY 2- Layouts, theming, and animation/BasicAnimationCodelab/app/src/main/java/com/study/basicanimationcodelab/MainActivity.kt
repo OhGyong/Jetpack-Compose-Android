67ae821c1d1bf3bb0d6edf1dfab90b259dc9eaec
O_Gyong <ogyong7143@gmail.com>
2023-05-25 15:08:59 +0900

    Basic Animation Codelab 프로젝트 생성

---
+++ PATHWAY 2- Layouts, theming, and animation/BasicAnimationCodelab/app/src/main/java/com/study/basicanimationcodelab/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.study.basicanimationcodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.study.basicanimationcodelab.ui.theme.BasicAnimationCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicAnimationCodelabTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
